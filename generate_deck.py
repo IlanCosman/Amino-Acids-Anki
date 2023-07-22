82605b1b683590debc999321e205454698f4fc3f
Ilan Cosman <ilancosman@gmail.com>
2023-07-22 07:17:26 -0700

    work

--- generate_deck.py
+++ generate_deck.py
@@ -1,4 +1,7 @@
 import dataclasses
+import enum
+
+import genanki
 
 import generate_svgs
 
@@ -10,6 +13,13 @@ class AminoAcid:
     name: str
     three_letter_code: str
     one_letter_code: str
+
+
+class AminoAcidCardField(enum.StrEnum):
+    NAME = "Name"
+    STRUCTURE = "Structure"
+    THREE_LETTER_CODE = "Three Letter Code"
+    ONE_LETTER_CODE = "One Letter Code"
 
 
 def generate_decks():
@@ -38,6 +48,35 @@ def generate_decks():
     )
 
     generate_svgs.generate_svgs(aa.name for aa in AMINO_ACIDS)
+
+    MODEL = genanki.Model(
+        1864258524,  # ID is unique, do not change
+        "Amino Acids Model",
+        fields=[
+            {"name": AminoAcidCardField.NAME},
+            {"name": AminoAcidCardField.STRUCTURE},
+            {"name": AminoAcidCardField.THREE_LETTER_CODE},
+            {"name": AminoAcidCardField.ONE_LETTER_CODE},
+        ],
+        templates=[
+            template(AminoAcidCardField.NAME, AminoAcidCardField.STRUCTURE),
+            template(AminoAcidCardField.STRUCTURE, AminoAcidCardField.NAME),
+            template(AminoAcidCardField.NAME, AminoAcidCardField.THREE_LETTER_CODE),
+            template(AminoAcidCardField.THREE_LETTER_CODE, AminoAcidCardField.NAME),
+            template(AminoAcidCardField.NAME, AminoAcidCardField.ONE_LETTER_CODE),
+            template(AminoAcidCardField.ONE_LETTER_CODE, AminoAcidCardField.NAME),
+        ],
+    )
+
+    DECK = genanki.Deck(1304768788, "Amino Acids")  # ID is unique, do not change
+
+
+def template(f1: AminoAcidCardField, f2: AminoAcidCardField) -> dict:
+    return {
+        "name": f"{f1} -> {f2}",
+        "qfmt": f"{{{f1}}}",
+        "afmt": f"{{FrontSide}}<hr id=answer>{{{f2}}}",
+    }
 
 
 if __name__ == "__main__":
